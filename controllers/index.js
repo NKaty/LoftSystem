dcfbf306617d13424293228a1fbd41f563ba952d
NKaty <nd.katy@gmail.com>
2018-04-05 19:51:20 +0300

    Update index.js

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const path = require('path');
-const { dist } = require('../config');
 
 exports.getIndex = async ctx => {
   ctx.body = fs.readFileSync(path.resolve(path.join('dist', 'index.html')), 'utf8');
